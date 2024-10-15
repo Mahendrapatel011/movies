8f5ddb47bc6ae275425f552c1edf44a99a82fbda
Mahendrapatel011 <mahendrapatel02220@gmail.com>
2024-10-15 17:49:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@mui/material', '@emotion/react', '@emotion/styled']
-    }
+  optimizeDeps: {
+    include: ['@mui/material', '@emotion/react', '@emotion/styled']
   }
 });
